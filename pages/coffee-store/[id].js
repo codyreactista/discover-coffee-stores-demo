3d83936e839767f48c8d96428d83b813e50dc53d
Cuong Phan <codyreactista@gmail.com>
2022-01-17 14:12:07 +0700

    Static Coffee Store Pages Section

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,97 @@
+import { fetchCoffeeStores } from "@lib/coffee-stores";
+import styles from "@styles/coffee-store.module.css";
+import cls from "classnames";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  const coffeeStores = await fetchCoffeeStores();
+
+  return {
+    props: {
+      coffeeStore: coffeeStores.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    }, // dynamic id
+  };
+}
+
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = coffeeStores.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const { address, name, imgUrl } = props.coffeeStore;
+
+  const handleUpvoteButton = () => {
+    console.log("handle upvote");
+  };
+
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/place.svg" width="24" height="24" />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width="24" height="24" />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeStore;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Banner from "@components/banner";
+import Card from "@components/card";
+import { fetchCoffeeStores } from "@lib/coffee-stores";
 import styles from "@styles/Home.module.css";
 import Head from "next/head";
 import Image from "next/image";
 
-export default function Home() {
+export async function getStaticProps(context) {
+  const coffeeStores = await fetchCoffeeStores();
+
+  return {
+    props: {
+      coffeeStores,
+    }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  console.log(
+    "🚀 ~ file: index.js ~ line 19 ~ Home ~ props",
+    props.coffeeStores
+  );
   const handleOnBannerBtnClick = () => {
     console.log("hi banner button");
   };
@@ -28,6 +44,24 @@ export default function Home() {
             alt="hero image"
           />
         </div>
+        {props.coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Ben Tre stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => (
+                <Card
+                  key={coffeeStore.id}
+                  name={coffeeStore.name}
+                  imgUrl={
+                    coffeeStore.imgUrl ||
+                    "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                  }
+                  href={`/coffee-store/${coffeeStore.id}`}
+                />
+              ))}
+            </div>
+          </div>
+        )}
       </main>
     </div>
   );
