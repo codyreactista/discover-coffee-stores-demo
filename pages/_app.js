53d095b861b940962b12f8cae1b26328c56e5b16
Cuong Phan <codyreactista@gmail.com>
2022-12-04 22:11:57 +0700

    added more optimized

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,25 @@
-import localFont from "@next/font/local";
+import { IBM_Plex_Sans } from "@next/font/google";
+import Image from "next/image";
 
 import StoreProvider from "@/store/store-context";
 import "@/styles/globals.css";
+import background from "public/static/background.png";
 
-const IBMPlexSans = localFont({
-  src: [
-    {
-      path: "../public/fonts/IBMPlexSans-Regular.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/IBMPlexSans-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/IBMPlexSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
 });
 
 function MyApp({ Component, pageProps }) {
   return (
     <StoreProvider>
-      <div className={IBMPlexSans.className}>
+      <div className={`${ibmPlexSans.className} container`}>
+        <Image
+          src={background}
+          alt="background image"
+          placeholder="blur"
+          fill
+        />
         <Component {...pageProps} />
       </div>
     </StoreProvider>
